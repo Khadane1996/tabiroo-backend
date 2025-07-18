cd8227c6710f11b7e733ed12114d890bf71dd73f
khalifa Sidibé <khalifasidibe@MacBook-Air-de-khalifa.local>
2025-07-18 16:29:25 +0000

    save

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,14 @@ use App\Mail\ConfirmRegistrationMail;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     Mail::to('ligrejutsu96@gmail.com')
+//     ->send(new ConfirmRegistrationMail('565789'));
+// });
+
+
 Route::get('/', function () {
-    Mail::to('ligrejutsu96@gmail.com')
-    ->send(new ConfirmRegistrationMail('565789'));
-    // return view('welcome');
+    return view('welcome');
 });
 
 
